4811fdcf8f9aff7b692284574b58c3aa66f87d22
rishabh0565 <rishabhsrivastava0565@gmail.com>
2022-08-19 22:39:55 +0530

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+
+public class Calculator {
+      private double a ; 
+      private double b ;
+      public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+      Calculator(double a , double b ){
+    	  this.a = a ; 
+    	  this.b = b ;
+      }
+      
+     public double sum () {
+    	return a+b ;  
+     }
+     public double sub () {
+     	return a-b ;  
+      }
+     public double mul () {
+     	return a*b ;  
+      }
+     public double div () {
+     	return a/b ;  
+      }
+     
+ }
