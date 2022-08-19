ba8c1145baba5b33be501d2f93651529de7bfae9
rishabh0565 <rishabhsrivastava0565@gmail.com>
2022-08-19 23:04:53 +0530

    second push

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
      public static void main(String[] args) {
 	   Calculator c = new Calculator(1,6);
 	   System.out.println(c.div());
+	   
+	   System.out.println(c.mul());
 	}
 }
